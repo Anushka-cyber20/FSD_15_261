a8e504c27fe8549191b40782ecbd9ae886b5a2a8
anushkacyber20 <anusri201107@gmail.com>
2026-09-01 11:56:56 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const os = require("os");
+console.log("Free Memory:", os.freeme());
+console.log("Total Memory:", os.totalmem());
+console.log("Platform:", os.platform());
+console.log("Architecture")
