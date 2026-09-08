78a39c247356bcf0a5afecd5704d4c79ed428c33
anushkacyber20 <anusri201107@gmail.com>
2026-09-08 12:13:40 +0530

    Added request method functionality

---
+++ src/req.js
@@ -0,0 +1,12 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World");
+  }
+});
+
+server.listen(8000, () => {
+  console.log("Server running at http://localhost:8000");
+});
